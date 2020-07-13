718ec6cc56b289ae2d1d4cfa0534452fcd07a3ac
Daniel Whitney <whitneyd@southwestern.edu>
2020-07-13 15:01:06 -0500

    added ip fetch

--- script.js
+++ script.js
@@ -8,13 +8,28 @@ const servers = [
   "172.16.1.11",
   "172.16.1.12",
 ];
-const serverIPs = [];
 
-const loadServers = () => {
+const serverIPs = [
+  "172.16.1.43",
+  "172.16.1.10",
+  "172.16.1.40",
+  "172.16.1.41",
+  "172.16.1.11",
+  "172.16.1.12",
+];
+
+async function loadServers() {
   servers.forEach((val) => {
     const markup = `<li>${val}</li>`;
     serverList.insertAdjacentHTML("beforeend", markup);
   });
-};
+  serverIPs.forEach((val) => {
+    fetch(`http://${val}`)
+      .then((res) => console.log(res))
+      .catch((error) => {
+        console.log(error);
+      });
+  });
+}
 
 window.addEventListener("load", loadServers);
