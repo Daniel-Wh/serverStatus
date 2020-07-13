d985536ec5746143b84511223327b6aa7fc71d2e
Daniel Whitney <whitneyd@southwestern.edu>
2020-07-13 11:52:46 -0500

    added servers list and styling

---
+++ script.js
@@ -0,0 +1,15 @@
+serversList = document.getElementById("serversList");
+
+const servers = [
+  "view.clearcube.com",
+  "cctaddc01.clearcube.local",
+  "m1.clearcube.local",
+  "m1-rds.clearcube.local",
+  "172.16.1.11",
+  "172.16.1.12",
+];
+const serverIPs = [];
+
+window.addEventListener("load", loadServers);
+
+const loadServers = () => {};
