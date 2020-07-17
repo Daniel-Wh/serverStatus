f608e21d516629589c8ff4d08d4083ad9edda96e
Laxmantim <fieldst@southwestern.edu>
2020-07-17 12:51:55 -0500

    added tcp port probing

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, jsonify
 import json
+import socket
 from threading import Timer
 from pythonping import ping
 from datetime import datetime as dt
@@ -13,6 +14,12 @@ serverIPs = ["172.16.1.43",
              "172.16.1.11",
              "172.16.1.12",
              "8.8.8.8"]
+
+
+
+server_ports = [80, 139, 443, 3389, 8080, 8443]
+
+
 
 # serverIPs = ["8.8.8.8", "172.16.1.43"]
 servers = []
@@ -36,11 +43,14 @@ def update_pings():
 @app.route('/update', methods=['GET'])
 def update_front_end():
     statuses = []
+    port_stats = []
     for server in servers:
         statuses.append(server.get_current_status())
+        port_stats.append(server.get_port_stats())
 
     return jsonify({
-        "statuses": statuses
+        "statuses": statuses,
+        "ports": port_stats
     })
 
 
@@ -49,12 +59,23 @@ class Server:
     is_up = True
     initialized = False
     status_message = ""
+    port_stats = []
+
+    BUFFER_SIZE = 2
+    #TCP_MSG = b'1'
+
 
     def __init__(self, ip):
         self.ip = ip
 
+    def get_ip(self): 
+        return self.ip
+
     def get_current_status(self):
         return self.status_message
+    
+    def get_port_stats(self):
+        return self.port_stats
 
     def check_ping(self):
         obj = ping(self.ip, verbose=False, count=1, timeout=1)
@@ -73,8 +94,34 @@ class Server:
                 if not self.is_up:
                     self.time = dt.now().strftime("%b %d %Y %H:%M")
                 self.is_up = True
+
+
+                self.port_stats.clear()
+
+                #s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                for port in server_ports:
+                    #print("trying port " + str(port) + " at " + self.ip)
+                    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    s.settimeout(0.1)
+                    try:
+                        s.connect((self.ip, port))
+                    except:
+                        self.port_stats.append(0)
+                        #print("port " + str(port) + " is closed")
+                    else:
+                        self.port_stats.append(1)
+                        #s.send(self.TCP_MSG)
+                        #data = s.recv(self.BUFFER_SIZE)
+                        s.close()
+   
+                        #print("connected successfully")
+
+
+
                 self.status_message = "Alive since " + self.time
+                print(self.ip)
                 print(self.status_message)
+                print(str(self.port_stats))
 
 
 def update_stored_pings(server_list):
