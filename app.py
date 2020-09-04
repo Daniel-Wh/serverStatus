8b38cfa1fc939b4cc34c850b9ff024bfd8bade1b
Daniel Whitney <whitneyd@southwestern.edu>
2020-09-04 17:48:38 -0500

    migrated to db, can add/remove at endpoints

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, jsonify, request
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
 import socket
 import webbrowser
 from threading import Timer
@@ -7,11 +8,13 @@ from pythonping import ping
 from datetime import datetime as dt
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
 CORS(app)
 
 serversFile = 'servers.txt'
 serverIPs = []
-
+initialized = False
 servernames = ["view.clearcube.com", 
                "cctaddc01.clearcube.local",
                "m1.clearcube.local",
@@ -23,7 +26,7 @@ servernames = ["view.clearcube.com",
 
 server_ports = [80, 139, 443, 3389, 8080, 8443]
 
-servers = []
+
 current_time = dt.now()
 print(current_time.strftime("%b %d %Y %H:%M"))
 webbrowser.open('http://127.0.0.1:5000')
@@ -35,43 +38,42 @@ def index():
 
 
 @app.before_first_request
-def update_pings():
-    servers_file = open(serversFile, 'r')
-    ips = servers_file.readlines()
-    servers_file.close()
-    for ip in ips[:-1]:
-        serverIPs.append(ip[:-1])
-
-    serverIPs.append(ips[len(ips)-1])
-    print(serverIPs)
-    print("opening log")
-    file_log = open("Status Log.txt", "a+") 
+def initialize_state():
+    db.create_all()
+    global initialized
+    file_log = open("Status Log.txt", "a+")
     file_log.write("\n \n ************************************************************* \n Monitor Started at")
     file_log.write(current_time.strftime("%b %d %Y %H:%M"))
+    # take base servers and add them to database
+    if not initialized:
+        servers_file = open(serversFile, 'r')
+        ips = servers_file.readlines()
+        servers_file.close()
+        initialized = True
+        for ip in ips[:-1]:
+            serverIPs.append(ip[:-1])
 
-    print("updated log")
+        serverIPs.append(ips[len(ips) - 1])
     for server in serverIPs:
         new_server = Server(server)
         new_server.check_ping()
-        servers.append(new_server)
+        new_server.save_to_db()
 
 
-def after_server_added(servername):
+def update_log(servername, message):
     file_log = open("Status Log.txt", "a+")
     file_log.write("\n \n ************************************************************* \n "
-                   "Server {} added at".format(servername))
+                   "Server {} {} at ".format(servername, message))
     file_log.write(current_time.strftime("%b %d %Y %H:%M"))
-    servers.clear()
-    for server in serverIPs:
-        new_server = Server(server)
-        new_server.check_ping()
-        servers.append(new_server)
+    server_list = Server.get_current_servers()
+    for server in server_list:
+        server.check_ping()
 
 
 def start_log():
     print("opening log")
     file_log = open("Status Log.txt", "a+") 
-    file_log.write("\n \n ************************************************************* \n Monitor Started at")
+    file_log.write("\n \n ************************************************************* \n Monitor Started at ")
     file_log.write(current_time.strftime("%b %d %Y %H:%M"))
     file_log.write("\n")
     print("updated log")
@@ -82,12 +84,13 @@ def start_log():
 def update_front_end():
     statuses = []
     port_stats = []
-    for server in servers:
+    server_list = Server.get_current_servers()
+    for server in server_list:
         statuses.append(server.get_current_status())
-        print(server.get_ip())
-        print(server.get_port_stats())
         port_stats.append(server.get_port_stats())
 
+    print(statuses)
+    print(port_stats)
     return jsonify({
         "statuses": statuses,
         "ports": port_stats
@@ -99,18 +102,20 @@ def add_server():
     try:
         params = request.json
         ip = params['ip']
-        if ip not in serverIPs:
-            servers_file = open(serversFile, 'a')
-            servers_file.write("\n")
-            servers_file.write(ip)
-            servers_file.close()
-            serverIPs.append(ip)
-            name = params['name']
-            servernames.append(name)
-            after_server_added(name)
-            return 'Server Added Successfully', 200
-        else:
-            return 'Server already being checked', 201
+        servers_list = Server.get_current_servers()
+        for server in servers_list:
+            if server.get_ip() == ip:
+                return 'Server already being checked', 201
+
+        new_server = Server(ip)
+        new_server.save_to_db()
+        servers_file = open(serversFile, 'a')
+        servers_file.write("\n")
+        servers_file.write(ip)
+        servers_file.close()
+        update_log(ip, "added")
+        return 'Server Added Successfully', 200
+
     except:
         return 'System Error', 500
 
@@ -119,51 +124,101 @@ def add_server():
 def remove_server():
     try:
         params = request.json
-
         ip = params['ip']
-        if ip not in serverIPs:
-            return "Server not being tracked", 201
+        current_servers = Server.get_current_servers()
+        for server in current_servers:
+            if ip == server.get_ip():
+                server.delete_from_db()
+                update_log(ip, "removed")
+                file = open(serversFile, 'w')
+                for ip in current_servers[:-2]:
+                    file.write(str(ip.get_ip()))
+                    file.write('\n')
+                file.write(current_servers[-2].get_ip())
+                file.close()
+                return "Server removed successfully", 200
 
-        serverIPs.remove(ip)
-        file = open(serversFile, 'w')
-        file.write('')
-        for server in serverIPs[:-1]:
-            file.write(server)
-            file.write('\n')
-        file.write(serverIPs[len(serverIPs)-1])
-        file.close()
-        servers.clear()
-        after_server_added(ip)
-        return 'Server removed Successfully', 200
+        return "Server not being tracked", 201
+
     except RuntimeError:
         return 'System Error', 500
 
 
-class Server:
+# ------------------------------------------------------------------- #
+# ------------- Server Class and database integration --------------- #
+# ------------------------------------------------------------------- #
+class Server(db.Model):
     server_ports = [80, 139, 443, 3389, 8080, 8443]
     BUFFER_SIZE = 2
     # TCP_MSG = b'1'
 
+    id = db.Column(db.Integer, primary_key=True)
+    ip = db.Column(db.String(16), nullable=False)
+    is_up = db.Column(db.Boolean)
+    last_state_change = db.Column(db.DateTime)
+    status_message = db.Column(db.String(40))
+    port_stats = db.Column(db.String(10), default='')
+    initial_state = db.Column(db.Boolean)
+
     def __init__(self, ip):
-        self.time = 0
         self.is_up = True
-        self.initialized = False
-        self.status_message = ""
-        self.port_stats = []
+        self.last_state_change = dt.now()
         self.ip = ip
+        self.initial_state = False
+        self.port_stats = ''
+
+    def set_port_stats(self, val):
+        self.port_stats += ',' + val
+
+    def get_port_stats(self):
+        int_list = []
+        for x in self.port_stats[1:].split(','):
+            int_list.append(int(x))
+        return int_list
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
 
     def get_ip(self): 
         return str(self.ip)
 
     def get_current_status(self):
         return self.status_message
-    
-    def get_port_stats(self):
-        return self.port_stats
+
+    def get_up(self):
+        return self.is_up
+
+    def get_initialized(self):
+        return self.initial_state
+
+    @classmethod
+    def delete_by_ip(cls, ip):
+        row = db.session.query(cls).filter(cls.ip == ip)
+        row.delete_from_db()
+
+    @classmethod
+    def state_change_set(cls, __id, new_state):
+        row = db.session.query(cls).filter(cls.id == __id)
+        row.last_state_change = new_state
+        db.session.commit()
+
+    @classmethod
+    def get_current_servers(cls):
+        server_list = []
+        row = db.session.query(cls)
+        for row in row:
+            server_list.append(row)
+
+        return server_list
 
     def check_ping(self):
         file_log = open("Status Log.txt", "a+") 
-        self.port_stats.clear()
+        self.port_stats = ''
         port_open = False
         for port in server_ports:
             # print("trying port " + str(port) + " at " + self.ip)
@@ -172,45 +227,52 @@ class Server:
             try:
                 s.connect((self.ip, port))
             except:
-                self.port_stats.append(0)
+                self.set_port_stats('0')
                 # print("port " + str(port) + " is closed")
             else:
                 port_open = True
-                self.port_stats.append(1)
+                self.set_port_stats('1')
                 s.close()
 
-        print(self.port_stats)
         obj = ping(self.ip, verbose=False, count=1, timeout=1)
-        if not self.initialized:
-            self.time = dt.now().strftime("%b %d %Y %H:%M")
-            self.initialized = True
+        if not self.initial_state:
+            print("initializiing " + self.ip)
+            self.last_state_change = dt.now()
+            self.time = self.last_state_change.strftime("%b %d %Y %H:%M")
+            self.initial_state = True
+            self.is_up = True
+            self.status_message = "Alive Since " + self.time
+
         for thing in obj:
             for_comparison = str(thing)
+            print(thing)
             # print(for_comparison)
             if for_comparison == "Request timed out" and not port_open:
                 if self.is_up:
-                    self.time = dt.now().strftime("%b %d %Y %H:%M")
-                    print("updating log")
+                    print("server was up but now is down")
+                    self.last_state_change = dt.now()
+                    # print("updating log")
                     file_log.write("\n")
-                    file_log.write(self.ip + " went down at " + self.time)
+                    file_log.write(self.ip + " went down at " + self.last_state_change.strftime("%b %d %Y %H:%M"))
                 self.is_up = False
-                self.status_message = "Dead since " + self.time
-                print(self.status_message)
+                self.status_message = "Dead since " + self.last_state_change.strftime("%b %d %Y %H:%M")
+                self.save_to_db()
             else:
                 if not self.is_up:
-                    self.time = dt.now().strftime("%b %d %Y %H:%M")
+                    self.last_state_change = dt.now()
                     file_log.write("\n")
-                    file_log.write(self.ip + " came back at " + self.time)
+                    file_log.write(self.ip + " came back at " + self.last_state_change.strftime("%b %d %Y %H:%M"))
                 self.is_up = True
-                self.status_message = "Alive since " + self.time
-
-        print(self.ip)
-        print(self.status_message)
-        print(str(self.port_stats))
+                self.status_message = "Alive since " + self.last_state_change.strftime("%b %d %Y %H:%M")
+                self.save_to_db()
+        # print(self.ip)
+        # print(self.port_stats)
+        # print(self.status_message)
         file_log.close()
 
 
-def update_stored_pings(server_list):
+def update_stored_pings():
+    server_list = Server.get_current_servers()
     for server in server_list:
         server.check_ping()
 
@@ -241,7 +303,7 @@ class RepeatedTimer(object):
         self.is_running = False
 
 
-rt = RepeatedTimer(60, update_stored_pings, servers)
+rt = RepeatedTimer(60, update_stored_pings)
 
 
 if __name__ == '__main__':
