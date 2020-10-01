ac1febec5907ac9c9cbabf9bfd029961f2279d8c
Daniel Whitney <whitneyd@southwestern.edu>
2020-10-01 11:04:53 -0500

    updated to capture invalid readings

--- DHTNode.js
+++ DHTNode.js
@@ -7,23 +7,27 @@ const getTemps = async ()=>{
   if(err){
     console.log(err)
   }
-  vals.temp = ""+ (temperature*1.8+32).toFixed(2)
+  vals.temp = ""+ (temperature*1.8+32).toFixed(1)
   vals.humi = ""+ humidity.toFixed(2);
-  const date = new Date(Date.now())
-  const dateForText = `${date.getFullYear()} ${date.getMonth() +1} ${date.getDate()} ${date.getHours()} ${date.getMinutes()} T`
-  console.log(date)
-  console.log(vals)
-  
-  fs.appendFile(`${__dirname}/readings.txt`, `${dateForText} ${vals.temp} ${vals.humi}\n`, (err) => {
-    if(err){
-      console.log(err)
-      }
-      else {
-        
-      fs.readFileSync(`${__dirname}/readings.txt`, "utf8")
+  if(vals.temp > 100 || vals.humi > 100){
+    console.log("invalid temperature or humidity")
+  }
+  else {
+    const date = new Date(Date.now())
+    const dateForText = `${date.getFullYear()} ${date.getMonth() +1} ${date.getDate()} ${date.getHours()} ${date.getMinutes()} T`
+    console.log(date)
+    console.log(vals)
+    
+    fs.appendFile(`${__dirname}/readings.txt`, `${dateForText} ${vals.temp} ${vals.humi}\n`, (err) => {
+      if(err){
+        console.log(err)
         }
-    } )
-  
+        else {
+          
+        fs.readFileSync(`${__dirname}/readings.txt`, "utf8")
+          }
+      } )
+  }
 });
 }
 
